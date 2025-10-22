0e4ee3ff69e7a16eb0c8f597eaee3f7fbf98f24c
Miguel Ángel Nieto Comino <mniecom1106@iesmarquesdecomares.org>
2025-10-22 09:23:51 +0200

    first commit

--- src/grupoACB/docriluc.java
+++ src/grupoACB/docriluc.java
@@ -3,7 +3,7 @@ package grupoACB;
 public class docriluc {
 
 	public static void main(String[] args) {
-		System.out.println("Miguel Ángel Nieto Comino");
+		System.out.println("Miguel Ángel Nieto Comino ");
 		
 
 	}
