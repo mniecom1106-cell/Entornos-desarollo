ccd63ac54bb5a8d962c8957302bbc914f840fe77
daniel <danieluser1909@gmail.com>
2025-10-27 13:39:31 +0100

    Peroles Commit

---
+++ src/grupoACB/peroles.java
@@ -0,0 +1,15 @@
+package grupoACB;
+
+public class peroles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		while (true) {
+			System.err.println();
+			
+		}
+		
+	}
+
+}
